4f295311fb1b7ea8c27907852398d9505346b8d2
beamedbyotter <108137884+beamedbyotter@users.noreply.github.com>
2022-06-24 09:10:08 -0500

    Create Program.cs

---
+++ terror v2/Program.cs
@@ -0,0 +1,18 @@
+// bro think he carti
+
+using System;
+using System.Windows.Forms;
+
+namespace E_Ware
+{
+  internal static class Program
+  {
+    [STAThread]
+    private static void Main()
+    {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run((Form) new Form1());
+    }
+  }
+}
