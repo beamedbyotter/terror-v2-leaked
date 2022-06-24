1d0bdbcef9ffc32ef2d779e3afd8da6e210601e5
beamedbyotter <108137884+beamedbyotter@users.noreply.github.com>
2022-06-24 09:09:49 -0500

    Create Functions.cs

---
+++ terror v2/Functions.cs
@@ -0,0 +1,16 @@
+// decompiled by otter
+
+using System.IO;
+using System.Windows.Forms;
+
+namespace E_Ware
+{
+  internal class Functions
+  {
+    public static void PopulateListBox(ListBox lsb, string Folder, string FileType)
+    {
+      foreach (FileInfo file in new DirectoryInfo(Folder).GetFiles(FileType))
+        lsb.Items.Add((object) file.Name);
+    }
+  }
+}
