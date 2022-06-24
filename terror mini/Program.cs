712058b764c16b084ac7e774d94b94c09dd7c134
beamedbyotter <108137884+beamedbyotter@users.noreply.github.com>
2022-06-24 09:13:28 -0500

    Create Program.cs

---
+++ terror mini/Program.cs
@@ -0,0 +1,18 @@
+// beamed by otter!!
+
+using System;
+using System.Windows.Forms;
+
+namespace E_Ware
+{
+  internal static class Program
+  {
+    [STAThread]
+    private static void Main()
+    {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run((Form) new Form1());
+    }
+  }
+}
